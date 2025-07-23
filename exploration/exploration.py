8cec5e9b30e01b590ca14470966ec5701a049cd2
Gabriel Santos <oliveirasgah@gmail.com>
2025-07-23 16:40:06 -0300

    change column name: Chuva to Chuva_mm

--- exploration/exploration.py
+++ exploration/exploration.py
@@ -72,14 +72,14 @@ for year in range(firstYear, currYear + 1):
             
         
         # Transform different target names to a unique name and remove NaN column names
-        columns_data = ['Chuva' if column == 'Chuva_mm' or column == 'Precip' else column for column in columns_data]
+        columns_data = ['Chuva_mm' if column == 'Chuva' or column == 'Precip' else column for column in columns_data]
         columns_data = [columns_data[i] + str(i) if columns_data[i] is not np.nan and columns_data.count(columns_data[i]) > 1 else columns_data[i] for i in range(0, len(columns_data))]
         columns_notna = [column for column in columns_data if column is not np.nan]
         
         data_nan_columns.columns = columns_data
         
         # Add valid columns to new dataframe
-        data_nan_columns = data_nan_columns[data_nan_columns['Chuva'].notnull()]
+        data_nan_columns = data_nan_columns[data_nan_columns['Chuva_mm'].notnull()]
         
         data_valid_columns = data_nan_columns[columns_notna].copy()
         data_valid_columns = data_valid_columns.drop_duplicates()
@@ -139,7 +139,6 @@ df_met['Horas'] = (
     df_met['Horas']
         .apply(lambda x: x - (int(x / 100) * 40) if not pd.isna(x) else x)
 )
-df_met.head(10)
 
 # %%
 # Create TIMESTAMP for old data
@@ -163,10 +162,63 @@ df_met['TIMESTAMP'] = (
         )
 )
 
-df_met['TIMESTAMP']
+# %%
+# Listing the current used columns in new entries
+columns = [
+    'TIMESTAMP',
+    'BattV_Avg',
+    'Tar_AVG',
+    'UR_inst',
+    'Vvento_ms_AVG',
+    'Dvento_G',
+    'Qg_AVG',
+    'PAR_AVG',
+    'Rn_Avg',
+    'Chuva_mm',
+    'Patm_kPa_AVG',
+    'rQg_AVG',
+    'Qatm_AVG',
+    'Qsup_AVG',
+    'Boc_AVG',
+    'Bol_AVG',
+    'Albedo_Avg',
+    'QatmC_AVG',
+    'QsupC_AVG',
+    'Vvento_ms_S_WVT',
+    'Dvento_D1_WVT',
+    'Dvento_SD1_WVT',
+    'PainelT'
+]
+df_met[columns]
 
 # %%
-df_met = df_met.drop(['Dia Juliano', 'Dia', 'Horas', 'Horario', 'Horário'], axis=1)
+'''
+# Variables
+
+TIMESTAMP - Date/time of measurement
+BattV_Avg - Battery voltage average (power supply monitoring)
+Tar_AVG - Air temperature average (°C)
+UR_inst - Relative humidity instantaneous (%)
+Vvento_ms_AVG - Wind velocity average (m/s)
+Dvento_G - Wind direction (degrees)
+Qg_AVG - Global solar radiation average (W/m² or MJ/m²/day) NrelScienceDirect
+PAR_AVG - Photosynthetically Active Radiation average (µmol/m²/s)
+Rn_Avg - Net radiation average - downward and upward fluxes of shortwave and longwave radiation Net Radiation - an overview | ScienceDirect Topics (W/m²)
+Chuva_mm - Rainfall (mm)
+Patm_kPa_AVG - Atmospheric pressure average (kPa)
+rQg_AVG - Reflected global radiation average (W/m²)
+Qatm_AVG - Atmospheric longwave radiation downward average (W/m²)
+Qsup_AVG - Surface longwave radiation upward average (W/m²)
+Boc_AVG - Bowen ratio or thermal sensor reading average
+Bol_AVG - Another thermal/radiation sensor average
+Albedo_Avg - Surface albedo average - fraction of sunlight reflected by surface (0-1 scale) Albedo - Wikipedia
+QatmC_AVG - Corrected atmospheric radiation average (W/m²)
+QsupC_AVG - Corrected surface radiation average (W/m²)
+Vvento_ms_S_WVT - Wind velocity scalar average with vector calculation
+Dvento_D1_WVT - Wind direction unit vector component
+Dvento_SD1_WVT - Wind direction standard deviation component
+PainelT - Panel temperature (°C) - likely data logger housing temperature
+'''
 
 # %%
-df_met.info(memory_usage='deep')
+df_met.info()
