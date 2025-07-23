6b2bbeeea4c308250a44e64ca7e5ad96a9e33f10
Gabriel Santos <oliveirasgah@gmail.com>
2025-07-23 16:50:02 -0300

    docs: variables meaning

--- exploration/exploration.py
+++ exploration/exploration.py
@@ -190,35 +190,3 @@ columns = [
     'PainelT'
 ]
 df_met[columns]
-
-# %%
-'''
-# Variables
-
-TIMESTAMP - Date/time of measurement
-BattV_Avg - Battery voltage average (power supply monitoring)
-Tar_AVG - Air temperature average (°C)
-UR_inst - Relative humidity instantaneous (%)
-Vvento_ms_AVG - Wind velocity average (m/s)
-Dvento_G - Wind direction (degrees)
-Qg_AVG - Global solar radiation average (W/m² or MJ/m²/day) NrelScienceDirect
-PAR_AVG - Photosynthetically Active Radiation average (µmol/m²/s)
-Rn_Avg - Net radiation average - downward and upward fluxes of shortwave and longwave radiation Net Radiation - an overview | ScienceDirect Topics (W/m²)
-Chuva_mm - Rainfall (mm)
-Patm_kPa_AVG - Atmospheric pressure average (kPa)
-rQg_AVG - Reflected global radiation average (W/m²)
-Qatm_AVG - Atmospheric longwave radiation downward average (W/m²)
-Qsup_AVG - Surface longwave radiation upward average (W/m²)
-Boc_AVG - Bowen ratio or thermal sensor reading average
-Bol_AVG - Another thermal/radiation sensor average
-Albedo_Avg - Surface albedo average - fraction of sunlight reflected by surface (0-1 scale) Albedo - Wikipedia
-QatmC_AVG - Corrected atmospheric radiation average (W/m²)
-QsupC_AVG - Corrected surface radiation average (W/m²)
-Vvento_ms_S_WVT - Wind velocity scalar average with vector calculation
-Dvento_D1_WVT - Wind direction unit vector component
-Dvento_SD1_WVT - Wind direction standard deviation component
-PainelT - Panel temperature (°C) - likely data logger housing temperature
-'''
-
-# %%
-df_met.info()
